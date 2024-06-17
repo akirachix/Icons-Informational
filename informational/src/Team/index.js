dfed140ad2edb914479b7ffc0a5a9d2f9f6f046d
Shalom-uwamahoro <shalomuwamahoro@gmail.com>
2024-06-17 18:22:31 +0300

    added the part of the body that details our team members and their roles

---
+++ informational/src/Team/index.js
@@ -0,0 +1,50 @@
+import './index.css';
+
+const Team = () =>{
+    return(
+        <div className='team-details'>
+
+          <h1> Our Team</h1>  
+
+          <section id='sec1'>
+            <div id='mwanasha'>
+            <img src='/Image/mwanasha.jpg' className='image' alt='Mwanasha'/>
+            <h3>Ali Mwanasha</h3>
+            <h4>Senior Product Manager</h4>
+            </div>
+
+            <div id='cynthia'>
+            <img src='/Image/cynthia.jpg' className='image' alt='Cynthia'/>
+            <h3>Cynthia Matsitsa</h3>
+            <h4>UI/UX  Designer and Researcher</h4>
+          </div>
+
+          </section>
+
+          <section id='sec2'>
+            
+            <div id='agnes'>
+            <img src='/Image/Agnes.jpg' className='image' alt='Agnes'/>
+            <h3>Agnes Auma</h3>
+            <h4>Data Scientist</h4>
+            </div>
+
+            <div id='shalom'>
+            <img src='/Image/shalom.jpg' className='image' alt='Agnes'/>
+            <h3>Shalom Uwamahoro</h3>
+            <h4>Backend Engineer</h4>
+            </div>
+
+            <div id='racheal'>
+            <img src='/Image/racheal.jpg' className='image' alt='Racheal'/>
+            <h3>Racheal Aber</h3>
+            <h4>Fullstack Engineer</h4>
+          </div>
+          </section>
+
+
+        </div>
+    )
+}
+
+export default Team;
