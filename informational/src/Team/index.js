365edbdbd307bbd6fffdacb20b1866a1f9263781
Shalom-uwamahoro <shalomuwamahoro@gmail.com>
2024-07-02 14:46:20 +0300

    modified the styles

--- informational/src/Team/index.js
+++ informational/src/Team/index.js
@@ -9,12 +9,12 @@ const Team = () =>{
           <section id='sec1'>
             <div id='mwanasha'>
             <img src='/Image/mwanasha.jpg' className='image' alt='Mwanasha'/>
-            <h3>Ali Mwanasha</h3>
+            <h3 className='h3a'>Ali Mwanasha</h3>
             </div>
 
             <div id='cynthia'>
             <img src='/Image/cynthia.jpg' className='image' alt='Cynthia'/>
-            <h3>Cynthia Matsitsa</h3>
+            <h3 className='h3b'>Cynthia Matsitsa</h3>
           </div>
 
           </section>
@@ -23,17 +23,17 @@ const Team = () =>{
             
             <div id='agnes'>
             <img src='/Image/Agnes.jpg' className='image' alt='Agnes'/>
-            <h3>Agnes Auma</h3>
+            <h3 className='h3c'>Agnes Auma</h3>
             </div>
 
             <div id='shalom'>
             <img src='/Image/shalom.jpg' className='image' alt='Agnes'/>
-            <h3>Shalom Uwamahoro</h3>
+            <h3 className='h3d'>Shalom Uwamahoro</h3>
             </div>
 
             <div id='racheal'>
             <img src='/Image/racheal.jpg' className='image' alt='Racheal'/>
-            <h3>Racheal Aber</h3>
+            <h3 className='h3e'>Racheal Aber</h3>
           </div>
           </section>
 
