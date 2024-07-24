eace57d6b76f62f6f2e775ca12082a02c038b70e
Ali-Mwanasha <alimwanashaomari@gmail.com>
2024-07-24 19:07:54 +0300

    made changes on the images

--- informational/src/App.js
+++ informational/src/App.js
@@ -1,8 +1,8 @@
-
 import LandingPage from "./LandingPage";
 import "./index.css";
 import Navbar from "./Navbar";
 import AboutUs from "./AboutUs";
+import Services from "./Services";
 
 
 function App(){
